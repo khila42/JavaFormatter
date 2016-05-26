184624aa54162a5ecf662cf4e6fc9e21a2b4f9d8
khila42 <nathanielhcmc@gmail.com>
2016-05-25 22:52:55 -0300

    Add files via upload
    Fixed issues of not printing out anything. Simply forgot to close the PrintWriter. Almost done formatting brackets now just need to work on closing brackets being improperly indented.

--- JavaFileFormatter.java
+++ JavaFileFormatter.java
@@ -1,13 +1,13 @@
 
 /**
- *
- * @author Nathaniel McIntyre (Khila/Khila42)
- */
+*
+* @author Nathaniel McIntyre (Khila/Khila42)
+*/
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-public class JavaFileFormatter 
+public class JavaFileFormatter
 {
 
     private Scanner in;
@@ -17,48 +17,66 @@ public class JavaFileFormatter
     {
         in = new Scanner(new File(fileIn));
         output = new PrintWriter(fileOut);
-        output.println("I'm working!!!!");
+        //output.println("I'm working!!!!");
+        //output.close( );
     }
     public static void main(String[] args)
     throws FileNotFoundException
     {
         // TODO code application logic here
-        Scanner kbd = new Scanner(System.in); 
+        Scanner kbd = new Scanner(System.in);
         System.out.println("What is the name of the file?");
         String fileIn = kbd.next();
         kbd.nextLine();
         System.out.println("What would you like to name the file for this"
-                           + " program to write to?");
+        + " program to write to?");
         String fileOut = kbd.next();
         JavaFileFormatter f = new JavaFileFormatter(fileIn, fileOut);
-        //f.formatBrackets();
+        f.formatBrackets();
     }
-    
+
     public void formatBrackets()
     {
         int indents = 0;
-        for(int i = 0; i <= indents; i++)
-        {
-            System.out.print("\t");
-        }
+        boolean lineEnd;
         while(in.hasNext())
         {
             String word = in.next();
+            lineEnd = false;
             if(word.equals("{"))
             {
-                output.println("\n" + word);
+                output.println();
+                for(int i = 0; i < indents; i++)
+                {
+                    output.print("    ");
+                }
+                output.print(word);
                 indents++;
+                lineEnd = true;
             }
             else if(word.equals("}"))
             {
-                output.println("\n" + word);
+                output.print("\n" + word);
                 indents--;
+                lineEnd = true;
             }
             else
             {
-                output.print(word);
+                output.print(word + " ");
+            }
+            if(word.contains(";"))
+            {
+                lineEnd = true;
+            }
+            if(lineEnd)
+            {
+                output.println();
+                for(int i = 0; i < indents; i++)
+                {
+                    output.print("    ");
+                }
             }
         }
+        output.close( );
     }
-    
 }
