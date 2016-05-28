5e245329eea1e5df69f8b429f860c0b8f1a41dc7
khila42 <nathanielhcmc@gmail.com>
2016-05-28 07:22:15 -0300

    Add files via upload
    Next part of the task is to prevent the file from wrapping

--- JavaFileFormatter.java
+++ JavaFileFormatter.java
@@ -1,15 +1,14 @@
-
 /**
- *
- * @author Nathaniel McIntyre (Khila/Khila42)
- */
+*
+* @author Nathaniel McIntyre (Khila/Khila42)
+*/
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-public class JavaFileFormatter 
+import java.util.ArrayList;
+public class JavaFileFormatter
 {
-
     private Scanner in;
     private PrintWriter output;
     public JavaFileFormatter(String fileIn, String fileOut)
@@ -17,48 +16,85 @@ public class JavaFileFormatter
     {
         in = new Scanner(new File(fileIn));
         output = new PrintWriter(fileOut);
-        output.println("I'm working!!!!");
+        //output.println("I'm working!!!!");
+        //output.close( );
     }
     public static void main(String[] args)
     throws FileNotFoundException
     {
         // TODO code application logic here
-        Scanner kbd = new Scanner(System.in); 
+        Scanner kbd = new Scanner(System.in);
         System.out.println("What is the name of the file?");
         String fileIn = kbd.next();
         kbd.nextLine();
         System.out.println("What would you like to name the file for this"
-                           + " program to write to?");
+        + " program to write to?");
         String fileOut = kbd.next();
         JavaFileFormatter f = new JavaFileFormatter(fileIn, fileOut);
-        //f.formatBrackets();
+        f.formatBrackets();
     }
-    
+
     public void formatBrackets()
     {
         int indents = 0;
-        for(int i = 0; i <= indents; i++)
-        {
-            System.out.print("\t");
-        }
+        boolean lineEnd;
         while(in.hasNext())
         {
             String word = in.next();
+            lineEnd = false;
+            if(word.equals("for"))
+            {
+                while(!word.equals("{"))
+                {
+                    output.print(word + " ");
+                    word = in.next();
+                }
+                lineEnd = true;
+            }
             if(word.equals("{"))
             {
-                output.println("\n" + word);
+                output.println();
+                for(int i = 0; i < indents; i++)
+                {
+                    output.print("    ");
+                }
+                output.print(word);
                 indents++;
+                lineEnd = true;
             }
             else if(word.equals("}"))
             {
-                output.println("\n" + word);
                 indents--;
+                output.print(word);
+                lineEnd = true;
             }
             else
             {
-                output.print(word);
+                output.print(word + " ");
+            }
+            if(word.contains(";"))
+            {
+                lineEnd = true;
+            }
+            if(lineEnd)
+            {
+                output.println();
+                if(in.hasNext("}"))
+                {
+                    for(int i = 0; i < indents - 1; i++)
+                    {
+                        output.print("    ");
+                    }
+                }
+                else
+                {
+                    for(int i = 0; i < indents; i++)
+                    {
+                        output.print("    ");
+                    }
+                }
             }
         }
+        output.close( );
     }
-    
 }
